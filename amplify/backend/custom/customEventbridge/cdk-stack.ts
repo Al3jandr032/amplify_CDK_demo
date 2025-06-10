e984c5ca632eb1171f775e76890f3ce4801dbe2f
Alex <pedrolop15@hotmail.com>
2025-06-10 02:09:22 -0600

    feat(eventbridge): ⚡️ add lambda trigger and invoke permissions

--- amplify/backend/custom/customEventbridge/cdk-stack.ts
+++ amplify/backend/custom/customEventbridge/cdk-stack.ts
@@ -1,49 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Fn, Stack } from 'aws-cdk-lib';
-import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
-import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
-import { Construct } from 'constructs';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as events from 'aws-cdk-lib/aws-events';
-import * as targets from 'aws-cdk-lib/aws-events-targets';
-import * as pipes from 'aws-cdk-lib/aws-pipes';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cdk from 'aws-cdk-lib'
+import { Fn, Stack } from 'aws-cdk-lib'
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper'
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref'
+import { Construct } from 'constructs'
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as sns from 'aws-cdk-lib/aws-sns'
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
+import * as events from 'aws-cdk-lib/aws-events'
+import * as targets from 'aws-cdk-lib/aws-events-targets'
+import * as pipes from 'aws-cdk-lib/aws-pipes'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 
 export class cdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
-    super(scope, id, props);
+    super(scope, id, props)
     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
     new cdk.CfnParameter(this, 'env', {
       type: 'String',
       description: 'Current Amplify CLI env name',
-    });
+    })
     /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
-    const amplifyProjectInfo = AmplifyHelpers.getProjectInfo();
+    const amplifyProjectInfo = AmplifyHelpers.getProjectInfo()
     const dependencies: AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this,
       amplifyResourceProps.category,
       amplifyResourceProps.resourceName,
       [{
         category: "api",
         resourceName: "notificationdemo"
+      },
+      {
+        category: "function",
+        resourceName: "feedbackProcessor"
       },]
-    );
-    const apiKey = cdk.Fn.ref(dependencies.api.notificationdemo.GraphQLAPIIdOutput);
-    
-    // const apiId = 'arn:aws:dynamodb:us-east-1:314146303616:table/Feedback-mflndjo63rg5rkc6esh37lx2j4-dev/stream/2025-06-06T22:04:52.960';
-    const apiName = 'notificationdemo';
-    const modelName = 'Feedback';
+    )
+    const apiId = cdk.Fn.ref(dependencies.api.notificationdemo.GraphQLAPIIdOutput)
+    const feedbackProcessorArn = cdk.Fn.ref(dependencies.function.feedbackProcessor.Arn)
+
+
+    const streamArn = Fn.importValue(
+      cdk.Fn.sub('${apiId}:GetAtt:FeedbackTable:StreamArn', {
+        apiId: apiId,
+      })
+    )
+    const apiName = 'notificationdemo'
+    const modelName = 'Feedback'
     
     // Dynamo table
-    const tableName = cdk.Fn.importValue(`api${apiName}:GetAtt:${modelName}Table:Name`);
-    const table = dynamodb.Table.fromTableName(this, `${modelName}Table`, tableName);
+    const tableName = cdk.Fn.importValue(`api${apiName}:GetAtt:${modelName}Table:Name`)
+    const table = dynamodb.Table.fromTableName(this, `${modelName}Table`, tableName)
 
     // EventBridge custom bus
     const bus = new events.EventBus(this, 'CustomBus', {
       eventBusName: `Demo-${amplifyProjectInfo.projectName}-${cdk.Fn.ref('env')}`,
-    });
+    })
 
     // IAM role for the pipe
     const pipeRole = new iam.Role(this, 'PipeRole', {
@@ -71,7 +82,7 @@ export class cdkStack extends cdk.Stack {
           ]
         })
       }
-    });
+    })
 
     const streamPolicy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
@@ -86,23 +97,23 @@ export class cdkStack extends cdk.Stack {
           TableArn: table.tableArn
         })
       ]
-    });
+    })
 
     // Permissions for Pipe to put events to bus
-    bus.grantPutEventsTo(pipeRole);
+    bus.grantPutEventsTo(pipeRole)
 
     // EventBridge Pipe from DynamoDB Stream to Bus
     const pipe = new pipes.CfnPipe(this, 'DDBStreamToBusPipe', {
       roleArn: pipeRole.roleArn,
-      // source: 'arn:aws:dynamodb:us-east-1:314146303616:table/Feedback-mflndjo63rg5rkc6esh37lx2j4-dev/stream/2025-06-06T22:04:52.960',
-      source: Fn.sub('arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/${modelName}-mflndjo63rg5rkc6esh37lx2j4-${stage}/stream/2025-06-06T22:04:52.960',
-        {
-          modelName: modelName,
-          appId: 'mflndjo63rg5rkc6esh37lx2j4',
-          stage: cdk.Fn.ref('env'),
-          streamLabel: '2025-06-06T22:04:52.960',
-        }
-      ),
+      source: streamArn,
+      // source: Fn.sub('arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/${modelName}-${appId}-${stage}/stream/${streamLabel}',
+      //   {
+      //     modelName: modelName,
+      //     appId: apiId,
+      //     stage: cdk.Fn.ref('env'),
+      //     streamLabel: '2025-06-06T22:04:52.960',
+      //   }
+      // ),
       sourceParameters: {
         dynamoDbStreamParameters: {
           startingPosition: 'LATEST',
@@ -115,7 +126,7 @@ export class cdkStack extends cdk.Stack {
           detailType: 'ddb-stream-record',
         },
       },
-    });
+    })
 
     const ddbStreamRule = new events.Rule(this, 'DDBStreamRule', {
       eventBus: bus, // Your custom EventBridge bus
@@ -137,22 +148,39 @@ export class cdkStack extends cdk.Stack {
           // }
         }
       }
-    });
+    })
+
+    // Lambda configuration
+    const feedbackProcessorFunction = lambda.Function.fromFunctionArn(
+      this, 
+      'feedbackProcessorFunction', 
+      feedbackProcessorArn
+    )
+
+   
+    new cdk.aws_lambda.CfnPermission(this, 'LambdaEventBridgePermission', {
+      functionName: feedbackProcessorFunction.functionName, // Use function name instead of ARN
+      action: 'lambda:InvokeFunction',
+      principal: 'events.amazonaws.com',
+      sourceArn: ddbStreamRule.ruleArn,
+    })
+
+    ddbStreamRule.addTarget(new targets.LambdaFunction(feedbackProcessorFunction))
 
     // SNS Configuration
-    const emailAddress = "alopez.biesa@gmail.com"
+    const emailAddress = "alex_demo@yopmail.com"
 
-    const snsTopicResourceName = `sns-topic-${amplifyProjectInfo.projectName}-${cdk.Fn.ref('env')}`;
+    const snsTopicResourceName = `sns-topic-${amplifyProjectInfo.projectName}-${cdk.Fn.ref('env')}`
     const topic = new sns.Topic(this, 'sns-topic', {
       topicName: snsTopicResourceName,
-    });
+    })
 
-    topic.addSubscription(new subs.EmailSubscription(emailAddress));
+    topic.addSubscription(new subs.EmailSubscription(emailAddress))
     new cdk.CfnOutput(this, 'snsTopicArn', {
       value: topic.topicArn,
       description: 'The arn of the SNS topic',
-    });
+    })
 
-    ddbStreamRule.addTarget(new targets.SnsTopic(topic));
+    ddbStreamRule.addTarget(new targets.SnsTopic(topic))
   }
 }
--- amplify/backend/function/feedbackProcessor/src/index.py
+++ amplify/backend/function/feedbackProcessor/src/index.py
@@ -1,15 +1,33 @@
 import json
 
+
+def print_pretty(event):
+    for key, value in event.items():
+        print(
+            f"  • {key}: {json.dumps(value, indent=2) if isinstance(value, dict) else value}"
+        )
+
+
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    print(context)
+    print("📦 Received Event:")
+    print_pretty(event)
+
+    detail_type = event.get("detail-type", "Unknown")
+    source = event.get("source", "Unknown")
+    detail = event.get("detail", {})
+
+    print(f"🔍 Event Type: {detail_type}")
+    print(f"🔗 Source: {source}")
+    print("🧾 Detail Payload:")
+    print_pretty(detail)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps("Hello from your new Amplify Python lambda!"),
+    }
